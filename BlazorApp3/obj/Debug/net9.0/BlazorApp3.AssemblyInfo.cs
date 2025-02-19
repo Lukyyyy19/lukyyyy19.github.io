2123e4f57d275500923bd55def89ed9a6c0fe22f
Lucas Gonzalez Ferreyra <lucasgf2002@gmail.com>
2025-02-19 01:20:41 -0300

    ;;;

--- BlazorApp3/obj/Debug/net9.0/BlazorApp3.AssemblyInfo.cs
+++ BlazorApp3/obj/Debug/net9.0/BlazorApp3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a71ef112cf8cfb0c3ae7b4b4df5df9e3d36f2fba")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
